74bcaa1f0272a74f61277f13533303ca24b8e84f
Collaborators <66287008+jpotesta10@users.noreply.github.com>
2024-04-23 06:39:16 -0400

    Create dbfilefilter.cpp

---
+++ dbfilefilter.cpp
@@ -0,0 +1,12 @@
+#include iostream
+#include exit
+#include teapotmath
+#include vectorphase
+
+struct Filter {
+name = formulavision
+def(): 
+favorite = name 
+sanitizeshine(favorite)
+return sanitieshine
+}
